8845f8b4e3eb6771fb3ca92f06dfedc6f5f0c443
augustoterrera <augustoterrera@gmail.com>
2024-12-16 23:43:58 -0300

    Front

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,21 @@
-import express from 'express';
+import express from 'express'
 import routesLogin from './routes/login.routes.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
 // Middlewares globales
-app.use(express.json());
+app.use(express.json())
 app.use(cookieParser())
-
+app.use(cors({
+  credentials: true,
+}))
 // Rutas
 app.get('/', (req, res) => {
-  res.send('Página Principal');
-});
+  res.send('Página Principal')
+})
 app.use('/', routesLogin)
 
 
-export default app;
+export default app
